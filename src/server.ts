8c197750cf9842637d544f50cebbe839fcce759c
Pedro Pimentel <pedropaulosv1@gmail.com>
2020-10-26 09:34:56 -0300

    Configs para deploy

--- src/server.ts
+++ src/server.ts
@@ -18,9 +18,20 @@ const app = express();
 //     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
 
-app.use(cors());
+var corsOptions = {
+    origin: function (origin, callback) {
+        // db.loadOrigins is an example call to load
+        // a list of origins from a backing database
+        db.loadOrigins(function (error, origins) {
+            callback(error, origins)
+        })
+    }
+}
+
+// app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
