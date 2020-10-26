7c54ea98d80064f44854d73372c3ad43280c9482
Pedro Pimentel <pedropaulosv1@gmail.com>
2020-10-26 09:56:21 -0300

    Config cors

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 import multer from 'multer';
+import cors from 'cors';
 
 import OrphanagesController from './controllers/OrphanagesControllers';
 import uploadConfig from './config/upload';
 
+var corsOptions = {
+    origin: 'https://happy-web-deploy.netlify.app/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 const routes = Router();
 const upload = multer(uploadConfig);
 
-routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages', cors(corsOptions), OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', upload.array('images'),OrphanagesController.create);
 
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import path from 'path';
 
 import 'dotenv/config';
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 
 import 'express-async-errors';
 
@@ -13,25 +13,15 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
-// var corsOptions = {
-//     origin: 'https://happy-backend-nlw.herokuapp.com/',
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-
-// var corsOptions = {
-//     origin: function (origin, callback) {
-//         // db.loadOrigins is an example call to load
-//         // a list of origins from a backing database
-//         db.loadOrigins(function (error, origins) {
-//             callback(error, origins)
-//         })
-//     }
-// }
+var corsOptions = {
+    origin: 'https://happy-web-deploy.netlify.app/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 // app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
+// app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
