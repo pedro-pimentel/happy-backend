3a07eb22273c84e896abc119f79b2447ad029398
Pedro Pimentel <pedropaulosv1@gmail.com>
2020-10-26 10:04:19 -0300

    Config cors

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,13 @@
 import { Router } from 'express';
 import multer from 'multer';
-import cors from 'cors';
 
 import OrphanagesController from './controllers/OrphanagesControllers';
 import uploadConfig from './config/upload';
 
-var corsOptions = {
-    origin: 'https://happy-web-deploy.netlify.app/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
 const routes = Router();
 const upload = multer(uploadConfig);
 
-routes.get('/orphanages', cors(corsOptions), OrphanagesController.index);
+routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', upload.array('images'),OrphanagesController.create);
 
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import path from 'path';
 
 import 'dotenv/config';
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 
 import 'express-async-errors';
 
@@ -13,12 +13,9 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
-// var corsOptions = {
-//     origin: 'https://happy-web-deploy.netlify.app/',
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-
-// app.use(cors());
+app.use(cors({
+    origin: 'https://happy-web-deploy.netlify.app'
+}));
 app.use(express.json());
 app.use(routes);
 // app.use(cors());
